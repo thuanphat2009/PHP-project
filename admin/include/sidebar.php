df1bff01261f50fa4a13db2170628a23e32b3548
thuanphat2009 <anhxanh2019@gmail.com>
2022-03-20 14:14:18 +0700

    sl ton kho user

--- admin/include/sidebar.php
+++ admin/include/sidebar.php
@@ -3,10 +3,10 @@
         Tip 1: You can change the color of the sidebar using: data-color="blue | green | orange | red | yellow"
     -->
       <div class="logo">
-        <a href="http://www.creative-tim.com" class="simple-text logo-mini">
+        <a href="/PHP_Project/index.php" class="simple-text logo-mini">
           CT
         </a>
-        <a href="http://www.creative-tim.com" class="simple-text logo-normal">
+        <a href="/PHP_Project/index.php" class="simple-text logo-normal">
           Creative Tim
         </a>
       </div>
