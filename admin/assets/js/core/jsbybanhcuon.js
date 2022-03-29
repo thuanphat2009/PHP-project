1743f4eac19c93e353978ac219369e35deae54e0
thuanphat2009 <anhxanh2019@gmail.com>
2022-03-30 00:38:31 +0700

    fix 1 chut

--- admin/assets/js/core/jsbybanhcuon.js
+++ admin/assets/js/core/jsbybanhcuon.js
@@ -1,5 +1,6 @@
 console.log(window.location.href)
-var href = "?action=logout";
-if((window.location.href).includes(href)){
+var href1 = "?action=logout";
+var href2 = "products.php?customer_id"
+if((window.location.href).includes(href1) || (window.location.href).includes(href2)){
     location.reload();
 }
--- success.php
+++ success.php
@@ -23,19 +23,20 @@ if (isset($_GET['partnerCode'])) {
 		$ngaylap =  date("Y/m/d");
 		$insert_order = "INSERT INTO tbl_order(id_khachhang,code_order,status,userName,phone,address,tongtien,ngaylap) VALUES('" . $id_khachhang . "','" . $code_order . "',1,'" . $name . "', '" . $phone . "','" . $address . "' ,'" . $tongtien . "','" . $ngaylap . "')";
 		$momo_query = mysqli_query($db->conn, $insert_order);
-
-		foreach ($_SESSION['cart'] as $key => $value) {
-			$id_sanpham = $value['id'];
-			$sanphamdat = $product->getproductbyId($id_sanpham);
-			$soluong = $value['soluong'];
-			$tonkhomoi = $value['tonkho'] - $soluong;
-			$daban = $value['daban'] + $value['soluong'];
-			$update_query = "UPDATE tbl_product SET 
-						daban = '$daban' ,tonkho = '$tonkhomoi' 
-						WHERE productId = '$id_sanpham'";
-			$insert_order_detail = "INSERT INTO tbl_order_detail(id_product,code_order,soluong) VALUES('" . $id_sanpham . "','" . $code_order . "','" . $soluong . "')";
-			mysqli_query($db->conn, $insert_order_detail);
-			mysqli_query($db->conn, $update_query);
+		if (isset($_SESSION['cart'])) {
+			foreach ($_SESSION['cart'] as $key => $value) {
+				$id_sanpham = $value['id'];
+				$sanphamdat = $product->getproductbyId($id_sanpham);
+				$soluong = $value['soluong'];
+				$tonkhomoi = $value['tonkho'] - $soluong;
+				$daban = $value['daban'] + $value['soluong'];
+				$update_query = "UPDATE tbl_product SET 
+							daban = '$daban' ,tonkho = '$tonkhomoi' 
+							WHERE productId = '$id_sanpham'";
+				$insert_order_detail = "INSERT INTO tbl_order_detail(id_product,code_order,soluong) VALUES('" . $id_sanpham . "','" . $code_order . "','" . $soluong . "')";
+				mysqli_query($db->conn, $insert_order_detail);
+				mysqli_query($db->conn, $update_query);
+			}
 		}
 	}
 	unset($_SESSION['cart']);
